d321f099d455a862f0fade191c4e8f5f226ba694
Matheus Pelozatto Sampaio <411658@unoesc.lan>
2024-03-25 22:08:03 -0300

    gerenciamento de clinica veterinaria

---
+++ Atividades/240325_01/Models/Veterinario.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _240325_01.Models
+{
+    public class Veterinario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+        public string? CRMV { get; set; }
+
+    }
+}
