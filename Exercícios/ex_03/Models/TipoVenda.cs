b2bb8ed95b4659af22aba30cb2c0cc96befd9635
Matheus <matheusp.sampaio@gmail.com>
2024-03-27 18:10:40 -0300

    classes dos imóveis

---
+++ Exercícios/ex_03/Models/TipoVenda.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace __ex_03.Models
+{
+    public class TipoVenda
+    {
+        public int Id { get; set; }
+        public string? ModeloVenda { get; set; }
+    }
+}
