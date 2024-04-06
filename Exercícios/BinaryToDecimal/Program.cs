47cd3a55587bd8108542c7b24042fce37b91dec4
Matheus <matheusp.sampaio@gmail.com>
2024-04-06 11:59:31 -0300

    atualizando o conversor de binário para decimal

--- Exercícios/BinaryToDecimal/Program.cs
+++ Exercícios/BinaryToDecimal/Program.cs
@@ -1,21 +1,28 @@
-﻿Queue<char> queueOfBinaryNum = new Queue<char>();
+﻿int BinaryToDecimal(string binaryNum)
+{
+    Queue<int> queueOfBinaryNum = new Queue<int>();
 
-Console.WriteLine("Digite um número binário: ");
+    foreach (var s in binaryNum)
+    {
+        queueOfBinaryNum.Enqueue(Convert.ToInt32(s));
+    }
+
+    double decimalNum = 0;
+
+    for (int totalElements = queueOfBinaryNum.Count; totalElements > 0; totalElements--)
+    {
+        int currentNum = queueOfBinaryNum.Dequeue();
+
+        if (currentNum == 49) 
+            decimalNum += Math.Pow(2, totalElements-1);
+    }
+
+    return Convert.ToInt32(decimalNum); 
+}
+
+Console.WriteLine("Número binário: ");
+
 string? binaryNum = Console.ReadLine();
-
-foreach (var s in binaryNum)
-{
-    queueOfBinaryNum.Enqueue(s);
-}
-
-double decimalNum = 0;
-
-for (int totalElements = queueOfBinaryNum.Count; totalElements > 0; totalElements--)
-{
-    int currentNum = Convert.ToInt32(queueOfBinaryNum.Dequeue().ToString());
-
-    if (currentNum == 1) 
-        decimalNum += Math.Pow(2, totalElements-1);
-}
+int decimalNum = BinaryToDecimal(binaryNum);
 
 Console.WriteLine($"Número decimal: {decimalNum}");
