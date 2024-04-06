6fe360b9a6edbd49d9ae1a128ed85b2e02747873
Matheus <matheusp.sampaio@gmail.com>
2024-04-06 11:33:31 -0300

    adicionando código do conversor de binário para decimal

---
+++ Exercícios/BinaryToDecimal/Program.cs
@@ -0,0 +1,21 @@
+﻿Queue<char> queueOfBinaryNum = new Queue<char>();
+
+Console.WriteLine("Digite um número binário: ");
+string? binaryNum = Console.ReadLine();
+
+foreach (var s in binaryNum)
+{
+    queueOfBinaryNum.Enqueue(s);
+}
+
+double decimalNum = 0;
+
+for (int totalElements = queueOfBinaryNum.Count; totalElements > 0; totalElements--)
+{
+    int currentNum = Convert.ToInt32(queueOfBinaryNum.Dequeue().ToString());
+
+    if (currentNum == 1) 
+        decimalNum += Math.Pow(2, totalElements-1);
+}
+
+Console.WriteLine($"Número decimal: {decimalNum}");
